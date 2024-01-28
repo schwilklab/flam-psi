4bb85b1b8d2e11c28357a33d25009c634df209e7
Azaj Mahmud <azmahmud@ttu.edu>
2024-01-28 16:49:23 -0600

    Modified comments in segemented.R

--- scripts/segmented.R
+++ scripts/segmented.R
@@ -30,13 +30,14 @@ dim(segmented_jupi_degsec) # 28
 
 range(segmented_jupi_degsec$ignition_delay) # Making sure the range of ignition delay is okay
 
+segmented_jupi_ig$ignition_delay <- log(segmented_jupi_ig$ignition_delay + 1)
 
 lm_jupi <- lm(ignition_delay ~ wp, data = segmented_jupi_ig)
 
 jupi_seg <- segmented::segmented(lm_jupi, seg.Z = ~ wp)
 
 
-AIC(lm_jupi, jupi_seg) # lm 522.9, segmented 523.9 (Linear model is better than segmented)
+AIC(lm_jupi, jupi_seg) 
 
 ####################################################################################
 # Now the wp vs temperature integration for Juniperus
@@ -46,8 +47,7 @@ lm_degsec_jupi <- lm(degsec_100 ~ wp, data = segmented_jupi_degsec)
 
 jupi_degsec_segmented <- segmented::segmented(lm_degsec_jupi, seg.Z = ~ wp)
 
-AIC(lm_degsec_jupi, jupi_degsec_segmented) # linear model is better than segmented model
-# Linear model is 677.7 and segmented is 681.3
+AIC(lm_degsec_jupi, jupi_degsec_segmented) # lm is better than segmented
 
 ###################################################################################
 # Prosopis glandulosa, initially ignition delay
@@ -68,14 +68,17 @@ dim(segmented_prgl2_degsec) # 35
 
 range(segmented_prgl2_degsec$ignition_delay)
 
+segmented_prgl2_ig$ignition_delay <- log(segmented_prgl2_ig$ignition_delay + 1)
+
 
 lm_prgl2 <- lm(ignition_delay ~ wp, data = segmented_prgl2_ig)
+
 prgl2_seg <- segmented::segmented(lm_prgl2, seg.Z = ~ wp)
 
 summary(prgl2_seg)
 
 
-AIC(lm_prgl2, prgl2_seg) # linear 162.8, segmented 157.2 (segmeneted model is better)
+AIC(lm_prgl2, prgl2_seg) 
 
 
 ####################################################################################
@@ -87,7 +90,7 @@ lm_degsec_prgl2 <- lm(degsec_100 ~ wp, data = segmented_prgl2_degsec)
 prgl2_degsec_seg <- segmented::segmented(lm_degsec_prgl2, seg.Z = ~ wp)
 
 
-AIC(lm_degsec_prgl2, prgl2_degsec_seg) # Linear 786.4, segmented 788.0 (linear is better)
+AIC(lm_degsec_prgl2, prgl2_degsec_seg) # lm is better than seg
 
 
 ###########################################################################
@@ -106,14 +109,17 @@ segmented_rhtr_degsec <- final_data %>%
   filter(spcode == "RHTR")
 
 dim(segmented_rhtr_degsec)
+
 range(segmented_rhtr_degsec$ignition_delay)
+
+segmented_rhtr_ig$ignition_delay <- log(segmented_rhtr_ig$ignition_delay + 1)
 
 
 lm_ig_rhtr <- lm(ignition_delay ~ wp, data = segmented_rhtr_ig)
 
 rhtr_ig_seg <- segmented::segmented(lm_ig_rhtr, seg.Z = ~ wp)
 
-AIC(lm_ig_rhtr, rhtr_ig_seg) # linear model 209.9, segmented 212.6 , linear model is better
+AIC(lm_ig_rhtr, rhtr_ig_seg) # lm is better than seg
 
 
 ####################################################################################
@@ -124,7 +130,7 @@ lm_degsec_rhtr <- lm(degsec_100 ~ wp, data = segmented_rhtr_degsec)
 
 rhtr_degsec_seg <- segmented::segmented(lm_degsec_rhtr, seg.Z = ~ wp)
 
-AIC(lm_degsec_rhtr, rhtr_degsec_seg) # Linear 700.6, segmented 702.7 (Linear model is better)
+AIC(lm_degsec_rhtr, rhtr_degsec_seg) # lm is better than seg
 
 
 ##########################################################################################
@@ -133,5 +139,4 @@ AIC(lm_degsec_rhtr, rhtr_degsec_seg) # Linear 700.6, segmented 702.7 (Linear mod
 
 rm("lm_degsec_rhtr", "rhtr_degsec_seg", "lm_ig_rhtr", "rhtr_ig_seg",
    "segmented_rhtr_degsec", "segmented_rhtr_ig", "lm_degsec_prgl2", "prgl2_degsec_seg",
-   "segmented_prgl2_degsec", "segmented_jupi_ig",  "segmented_jupi_degsec",
-   "lm_jupi", "jupi_seg", "lm_degsec_jupi", "jupi_degsec_segmented")
+   "segmented_prgl2_degsec",  "segmented_jupi_degsec", "lm_degsec_jupi", "jupi_degsec_segmented")
